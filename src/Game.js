22f62974f0bb7caf24395db157320fd8a3123768
ThuyNT13 <ngo.thuy38@gmail.com>
2018-12-09 14:32:26 -0800

    Implement move history capture and render; add key

--- src/Game.js
+++ src/Game.js
@@ -9,12 +9,13 @@ class Game extends Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
-  handleCick(i) {
-    const history = this.state.history;
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -28,14 +29,32 @@ class Game extends Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move# " +move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+    
     let status;
 
     if (winner) {
@@ -49,12 +68,12 @@ class Game extends Component {
         <div className="game-board">
           <Board 
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick = {(i) => this.handleClick(i)}
           />
         </div> 
         <div className="game-info">
           <div> {status} </div> 
-          <ol> { /* TODO */ } </ol> 
+          <ol> {moves} </ol> 
         </div> 
       </div>
     );
